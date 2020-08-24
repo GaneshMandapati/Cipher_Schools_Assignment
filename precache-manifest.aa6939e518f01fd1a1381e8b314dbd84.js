bcd70133c756bf87b7be7f1b98f5e6624b6e5186
Ganesh <mandapatiganesh389@gmail.com>
2020-08-24 19:05:08 +0530

    Updates

---
+++ precache-manifest.aa6939e518f01fd1a1381e8b314dbd84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf5f7ac386196e644d54ac20be21af9a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6f91ed2fae863e3c4d65",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "653081b9d37e49d0bf28",
+    "url": "./static/js/2.4da34c69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.4da34c69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f91ed2fae863e3c4d65",
+    "url": "./static/js/main.11117450.chunk.js"
+  },
+  {
+    "revision": "706c01f795c876eacc56",
+    "url": "./static/js/runtime-main.3cf45a73.js"
+  }
+]);
